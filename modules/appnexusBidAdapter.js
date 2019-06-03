ee8f54dd07569480258cb922163c8c24a5589a30
Peter Kim <peter@shareably.net>
2019-06-02 22:41:46 -0700

    appnexus bid adapter MAX_IMPS_PER_REQUEST = 10

--- modules/appnexusBidAdapter.js
+++ modules/appnexusBidAdapter.js
@@ -31,7 +31,7 @@ const NATIVE_MAPPING = {
   displayUrl: 'displayurl'
 };
 const SOURCE = 'pbjs';
-const MAX_IMPS_PER_REQUEST = 15;
+const MAX_IMPS_PER_REQUEST = 10;
 const mappingFileUrl = '//acdn.adnxs.com/prebid/appnexus-mapping/mappings.json';
 
 export const spec = {
