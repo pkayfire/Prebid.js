438f97cdd935403b09a90dbb73068d0ca94646e3
Kevin Xie <kevin.xie@shareably.net>
2021-01-25 14:45:38 -0800

    Fix eslint errors

--- modules/ixBidAdapter.js
+++ modules/ixBidAdapter.js
@@ -21,8 +21,8 @@ const USER_SYNC_URL = 'https://js-sec.indexww.com/um/ixmatch.html';
 
 function chunkArrayInGroups(arr, size) {
   var myArray = [];
-  for(var i = 0; i < arr.length; i += size) {
-    myArray.push(arr.slice(i, i+size));
+  for (var i = 0; i < arr.length; i += size) {
+    myArray.push(arr.slice(i, i + size));
   }
   return myArray;
 }
